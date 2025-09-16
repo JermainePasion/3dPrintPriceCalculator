584fb966090965c44c5b5bf82ed6040728a1008a
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2025-09-16 20:55:07 +0800

    frontend tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactTyped } from 'react-typed'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex justify-center px-4">
+      <h1 className="header-font text-[#9ACBD0] text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl mt-6 text-center">
+        <ReactTyped
+          strings={[
+            "3D PRINTING CALCULATOR",
+            "WITH A.I. CHATBOT",
+            "SAVE YOUR CALCULATIONS!"
+          ]}
+          typeSpeed={60}
+          backSpeed={40}
+          loop
+        />
+      </h1>
+    </div>
     </>
   )
 }
